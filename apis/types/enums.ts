5ec4a81f3880bbd3e487066fa3fdccaaec27f64a
_Kerman <kermanx@qq.com>
2023-01-14 09:17:12 +0000

    update enums

--- apis/types/enums.ts
+++ apis/types/enums.ts
@@ -6,5 +6,20 @@ export const enums = createEnums({
         INSIDE: 1,
         OUTSIDE: 2,
         LARGE: 3
+    },
+    Status: {
+        _type: int(),
+        WAITING_FOR_SIGNUP_AUDIT: 1,
+        UNSUBMITTED: 2,
+        WAITING_FOR_FIRST_AUDIT: 3,
+        WAITING_FOR_FINAL_AUDIT: 4,
+        ACCEPTED: 5,
+        REJECTED: 6,
+    },
+    NoticeType: {
+        _type: int(),
+        USER_NOTICE: 1,
+        CLASS_NOTICE: 2,
+        SCHOOL_NOTICE: 3,
     }
 })
