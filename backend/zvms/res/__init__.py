f47c2841edb0306d29da037c06c61349a6664169
_Kerman <kermanx@qq.com>
2023-01-14 09:17:44 +0000

    fix: wrong path

--- backend/zvms/res/__init__.py
+++ backend/zvms/res/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
 if sys.platform == 'win32':
-    STATIC_FOLDER = 'C:\zcvms-backend'
+    STATIC_FOLDER = 'C:\\zvms-backend'
 elif sys.platform == 'linux':
     STATIC_FOLDER = '/tmp/zvms-backend'
