a91d4fa42362a389e38778d40dca2c0145e6bda1
_Kerman <kermanx@qq.com>
2023-01-17 08:41:40 +0000

    generate structs for frontend

--- web/src/apis/types/structs.ts
+++ web/src/apis/types/structs.ts
@@ -1,14 +1,43 @@
-export interface Notice {
+import * as enums from "./enums.js";
+export interface NoticeBody {
+  title: string;
   content: string;
-  announcer: string;
-  time: string;
+  deadtime: string;
+}
+export interface Notice {
+  title: string;
+  content: string;
+  deadtime: string;
+  type: number;
+  targets: Array<number>;
+}
+export interface Report {
+  content: string;
+}
+export interface VolunteerRecordClass {
   id: number;
+  max: number;
 }
 export interface VolunteerRecord {
-  volId: number;
   name: string;
-  inside: number;
-  outside: number;
-  large: number;
-  status: number;
+  description: string;
+  time: string;
+  type: number;
+  reward: number;
+  classes: Array<VolunteerRecordClass>;
+}
+export interface UserOfUsers {
+  id: number;
+  name: string;
+  cls: number;
+  auth: number;
+  pwd: string;
+}
+export interface Users {
+  users: Array<UserOfUsers>;
+}
+export interface User {
+  name: string;
+  cls: number;
+  auth: number;
 }
