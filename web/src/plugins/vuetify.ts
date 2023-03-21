582881f3d6ca28133fb2463cec947352f2449a6c
zz_clc <15906672600@163.com>
2023-03-21 17:04:27 +0800

    update

--- web/src/plugins/vuetify.ts
+++ web/src/plugins/vuetify.ts
@@ -8,6 +8,12 @@ import { aliases, mdi } from "vuetify/iconsets/mdi";
 import "@mdi/font/css/materialdesignicons.css";
 //import { VDataTable } from "vuetify/labs/VDataTable";
 
+/*
+function decToHex(r: number,g: number,b: number) {
+  return '#'+;
+}
+*/
+
 export default createVuetify({
   components: {
     ...components,
@@ -25,5 +31,39 @@ export default createVuetify({
   locale: {
     locale: 'zhHans',
     messages: { zhHans, pl }
-  }
+  },
+
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          color1: '#CA0808',//decToHex(202,8,8),
+          color2: '#B21919',
+          color3: '#B33232',
+          color4: '#891F1F',
+          color5: '#33333F',
+          color6: '#6E6E76',
+          color7: '#766E6E',
+          color8: '#524040',
+          color9: '#5F2817',
+          color10: '#D98640',
+          
+        }
+      },
+    },
+  },
+  /*
+  202 8 8 
+  178 25 25
+  179 50 50
+  137 31 31
+
+  51 51 63
+  110 110 118
+  118 118 110
+  82 64 64
+  95 40 23
+  217 134 64
+  */
 });
